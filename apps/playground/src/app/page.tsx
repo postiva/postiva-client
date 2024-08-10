50905a39828f945c158b46ac038cd6e37fe3844e
Ali Osman Delismen <dlsmnosman@gmail.com>
2024-08-11 00:19:15 +0300

    fix: build errors

--- apps/playground/src/app/page.tsx
+++ apps/playground/src/app/page.tsx
@@ -4,8 +4,6 @@ import { postivaClient } from "@/libs/postiva";
 export default async function Home() {
   const posts = await postivaClient.contents.getContents();
 
-  const test = await postivaClient.contents.clap("z7g35qz63a52cj5562z5xboj");
-
   return (
     <div>
       <h1>Postiva Playground</h1>
